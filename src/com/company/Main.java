4423229a82db49098287daf92bbaeac45bd37513
Чуркин Василий <v.churkin@msk.i-teco.ru>
2019-02-05 13:05:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String todoName;
+        String todoDescription;
+
+        Map<String, String> todoList = new HashMap<>();
+        todoList.put("1", "aaa");
+        todoList.put("2", "bbb");
+        todoList.put("3", "ccc");
+
+        /*Integer id;
+        Map<Integer, Todo> todoList2 = new HashMap<>();
+        todoList2.put(id++, new Todo("1", "aaa"));
+        todoList2.put(id++, new Todo("2", "bbb"));
+        todoList2.put(id++, new Todo("3", "ccc"));*/
+
+        System.out.println("enter code: -c for create, -r for read, -u for update, -d for remove");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String code = reader.readLine();
+
+        while (true) {
+            switch (code) {
+                case "-c":
+                    todoName = reader.readLine();
+                    if (!todoList.containsKey(todoName)){
+                        todoDescription = reader.readLine();
+                        todoList.put(todoName, todoDescription);}
+                    else System.out.println("existing name");
+                    break;
+                case "-r":
+                    todoName = reader.readLine();
+                    if (todoList.containsKey(todoName)){
+                        System.out.println(todoList.get(todoName));}
+                    else System.out.println("no such name. cannot read");
+                    break;
+                case "-u":
+                    todoName = reader.readLine();
+                    todoDescription = reader.readLine();
+                    todoList.put(todoName, todoDescription);
+                    break;
+                case "-d":
+                    todoName = reader.readLine();
+                    if (todoList.containsKey(todoName)){
+                        todoList.remove(todoName);}
+                    else System.out.println("no such name. cannot remove");
+                    break;
+                case "exit":
+                    return;
+                case "show all":
+                    System.out.println(todoList.toString());
+                    break;
+                default:
+                    System.out.println("unknown command");
+                    break;
+            }
+            code = reader.readLine();
+        }
+
+    }
+}
+
